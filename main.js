26f8ff97eda9b52054c1a218da0551dc023b0418
estefatapias99 <32174302+estefatapias99@users.noreply.github.com>
2018-04-05 00:51:16 -0500

    slid

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+  $(".Modern-Slider").slick({
+    prevArrow:'<button class="PrevArrow"></button>',
+    nextArrow:'<button class="NextArrow"></button>',
+  });
+
+})
