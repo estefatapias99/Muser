bf5327b03dc85312a314c167900b7833989fe456
estefatapias99 <32174302+estefatapias99@users.noreply.github.com>
2018-04-04 23:23:57 -0500

    repositorio
    ...

---
+++ manual.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <title>Manual de Usuario</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="shortcut icon" href="views/assets/image/logo.png">
+    <link rel="stylesheet" href="manual.css">
+
+  </head>
+
+  <body>
+    <div class="super-container-one">
+    <div class="container--right">
+      <h1 class="ped"><p><a href="#">Pedidos</a></h1>
+      <h1 class="cot"><p><a href="#">Cotizaciones</a><h1>
+      <h1 class="pro"><p><a href="#">Productos</a><h1>
+      <h1 class="cont"><p><a href="#">Contacto</a><h1>
+      <h1 class="perf"><p><a href="#">Perfil</a><h1>
+      <h1 class="chat"><p><a href="#">Chat</a><h1>
+    </div>
+    <div class="container--center">
+      <h1 class="title">Manual de Usuario</h1>
+      <h2 class="subtitle">Maxirecargas S.A.S</h2>
+      <a href="maxirecargas"><button type="submit" name="button">Ir a Maxirecargas</button></a>
+    </div>
+</div>
+
+  <div class="row fold featured-fold featured-fold-es" >
+            <h1 class="title-prin">Utilización de la página web</h1>
+            <div class="featured-columns">
+                <div class="featured-border"></div>
+                <div class="featured-column featured-column-2">
+                    <h3>Ventaja</h3>
+                    <p>Guiarte para la correcta utilización del sistema de información, en la gestión de ventas online, cotizaciones, mantenimiento de equipos, recargas de toner y cartuchos; estableciendo así, los pasos necesarios para que conozcas sus diferentes funcionalidades.</p>
+                </div>
+                <div class="featured-border"></div>
+                <div class="featured-column featured-column-2">
+                    <h3>Mejora tus búsquedas</h3>
+                    <p>Encuentra todo lo que estás buscando en el menor tiempo posible, haciendo uso de los módulos que tenemos preparados para ti.</p>
+                </div>
+                <div class="featured-border"></div>
+                <div class="featured-column featured-column-2">
+                    <h3>Asistencia virtual</h3>
+                    <p>Podrás tener acceso a un chat para comunicarte directamente con un asesor de la empresa Maxirecargas, el cual estará para ti cuando tengas alguna inquietud en el horario de atención indicado.</p>
+                </div>
+                <div class="featured-border"></div>
+            </div>
+        </div>
+
+        <div class="super-container-two">
+
+
+
+        </div>
+
+
+
+
+
+
+
+
+
+  <script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
+
+  </body>
+</html>
