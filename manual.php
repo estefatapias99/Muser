8209d96c3964a0889f8833bee5c200b0ef84420f
evelinFrontend <ginatha97@gmail.com>
2018-04-07 17:44:37 -0500

    adwefsdg

--- manual.php
+++ manual.php
@@ -2,114 +2,60 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Manual de Usuario</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="shortcut icon" href="views/assets/image/logo.png">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- <link rel="shortcut icon" href="views/assets/image/logo.png"> -->
     <link rel="stylesheet" href="manual.css">
-
   </head>
   <body>
-    <div class="super-container-one">
-    <div class="container--right">
-      <h1 class="ped"><p><a href="#">Pedidos</a></h1>
-      <h1 class="cot"><p><a href="#">Cotizaciones</a><h1>
-      <h1 class="pro"><p><a href="#">Productos</a><h1>
-      <h1 class="cont"><p><a href="#">Contacto</a><h1>
-      <h1 class="perf"><p><a href="#">Perfil</a><h1>
-      <h1 class="chat"><p><a href="#">Chat</a><h1>
-    </div>
-    <div class="container--center">
-      <h1 class="title">Manual de Usuario</h1>
-      <h2 class="subtitle">Maxirecargas S.A.S</h2>
-      <a href="maxirecargas"><button type="submit" name="button">Ir a Maxirecargas</button></a>
-    </div>
-</div>
+    <!-- <div class="section one"></div> -->
+    <div id="carouseOrder" class="carousel slide" data-ride="carousel">
+      <ol>
+        <li data-target="carouseOrder" data-slide-to="0" class="active"></li>
+        <li data-target="carouseOrder" data-slide-to="1"></li>
+        <li data-target="carouseOrder" data-slide-to="2"></li>
+        <li data-target="carouseOrder" data-slide-to="3"></li>
+        <li data-target="carouseOrder" data-slide-to="4"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="carousel-item active">
+          <div class="content one">
 
-  <div class="row fold featured-fold featured-fold-es" >
-            <h1 class="title-prin">Utilización de la página web</h1>
-            <div class="featured-columns">
-                <div class="featured-border"></div>
-                <div class="featured-column featured-column-2">
-                    <h3>Ventaja</h3>
-                    <p>Guiarte para la correcta utilización del sistema de información, en la gestión de ventas online, cotizaciones, mantenimiento de equipos, recargas de toner y cartuchos; estableciendo así, los pasos necesarios para que conozcas sus diferentes funcionalidades.</p>
-                </div>
-                <div class="featured-border"></div>
-                <div class="featured-column featured-column-2">
-                    <h3>Mejora tus búsquedas</h3>
-                    <p>Encuentra todo lo que estás buscando en el menor tiempo posible, haciendo uso de los módulos que tenemos preparados para ti.</p>
-                </div>
-                <div class="featured-border"></div>
-                <div class="featured-column featured-column-2">
-                    <h3>Asistencia virtual</h3>
-                    <p>Podrás tener acceso a un chat para comunicarte directamente con un asesor de la empresa Maxirecargas, el cual estará para ti cuando tengas alguna inquietud en el horario de atención indicado.</p>
-                </div>
-                <div class="featured-border"></div>
-            </div>
+          </div>
         </div>
+        <div class="carousel-item active">
+          <div class="content two">
 
-<<<<<<< HEAD
-<!-- <div class="super-container-two">
-</div> -->
-    <div class="Modern-Slider">
-    <!-- Item -->
-    <div class="item">
-    <div class="img-fill">
-    <img src="https://i.imgur.com/JNKiMHU.jps" alt="">
-    <div class="info">
-      <div>
-        <h3>Full Width Slider With Layers</h3>
-        <h5>You can easily add image, html formatted texts and video layers over each slide and each layer accepts unique animation.</h5>
+          </div>
+        </div>
+        <div class="carousel-item active">
+          <div class="content three">
+
+          </div>
+        </div>
+        <div class="carousel-item active">
+          <div class="content">
+
+          </div>
+        </div>
+        <div class="carousel-item active">
+          <div class="content">
+
+          </div>
+        </div>
       </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+
     </div>
-    </div>
-    </div>
-    <!-- // Item -->
-    <!-- Item -->
-    <div class="item">
-    <div class="img-fill">
-    <img src="https://i.imgur.com/ESMjChq.jpg" alt="">
-    <div class="info">
-      <div>
-        <h3>Fully responsive Scales with its container</h3>
-        <h5>Donec id ornare dui. Aenean tristique condimentum elit, quis blandit nisl varius sit amet. Sed eleifend felis quis massa viverra</h5>
-      </div>
-    </div>
-    </div>
-    </div>
-    <!-- // Item -->
-    <!-- Item -->
-    <div class="item">
-    <div class="img-fill">
-    <img src="https://i.imgur.com/TDxSvHH.jpg" alt="">
-    <div class="info">
-      <div>
-        <h3>Awesome Transtions With CSS3</h3>
-        <h5>Donec id ornare dui. Aenean tristique condimentum elit, quis blandit nisl varius sit amet. Sed eleifend felis quis massa viverra</h5>
-      </div>
-    </div>
-    </div>
-    </div>
-    <!-- // Item -->
-    <!-- Item -->
-    <div class="item">
-    <div class="img-fill">
-    <img src="https://i.imgur.com/p1XZ3Mu.jpg" alt="">
-    <div class="info">
-      <div>
-        <h3>Separate settings per breakpoint</h3>
-        <h5>Donec id ornare dui. Aenean tristique condimentum elit, quis blandit nisl varius sit amet. Sed eleifend felis quis massa viverra</h5>
-      </div>
-    </div>
-    </div>
-    </div>
-    <!-- // Item -->
-    </div>
-  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-  <script src='https://kenwheeler.github.io/slick/slick/slick.js'></script>
-=======
-<div class="super-container-two">
-</div>
 
 
 
@@ -122,8 +68,10 @@
 
 
 
->>>>>>> e95059c5b63ba375b467a2724ac6385f421f9e43
-  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   <script type="text/javascript" src="main.js"></script>
   </body>
 </html>
