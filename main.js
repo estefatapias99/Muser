519ea6a1f79fde4691d63a7210cb609f375c35f4
estefatapias99 <32174302+estefatapias99@users.noreply.github.com>
2018-04-10 02:31:52 -0500

    ...--

--- main.js
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-
-=======
-$('#carouseOrder').carousel({
-  interval: 2000
-})
->>>>>>> 8209d96c3964a0889f8833bee5c200b0ef84420f
--- manual.php
+++ manual.php
@@ -50,6 +50,7 @@
 
 <div class="three"><p></p></div>
 
+<!-- PRODUCTOS -->
 <div id="slider" class="carousel slide" data-ride="carousel">
   <ul class="carousel-indicators">
     <li data-target="#slider" data-slide-to="0" class="active"></li>
@@ -58,9 +59,9 @@
   </ul>
   <div class="carousel-inner" >
     <div class="carousel-item active">
-      <!-- <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/Siberian-husky.jpg" alt="Los Angeles" width="1100"> -->
       <div class="slider-1">
-        <h3 class="title-ped">Productos</h3>
+        <img src="image/custom/productofondo.jpg" alt="">
+        <h3 class="title-ped">Encontrar Productos</h3>
         <!-- <p>Cómo realizar pedidos</p> -->
       </div>
     </div>
@@ -89,34 +90,97 @@
 
 <div class="three"><p></p></div>
 
+
+<!-- COTIZACIONES -->
 <div id="slider1" class="carousel slide" data-ride="carousel">
   <ul class="carousel-indicators">
     <li data-target="#slider1" data-slide-to="0" class="active"></li>
     <li data-target="#slider1" data-slide-to="1" ></li>
     <li data-target="#slider1" data-slide-to="2"></li>
+    <li data-target="#slider1" data-slide-to="3"></li>
+    <li data-target="#slider1" data-slide-to="4"></li>
+    <li data-target="#slider1" data-slide-to="5"></li>
   </ul>
   <div class="carousel-inner" >
     <div class="carousel-item active">
-      <!-- <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/Siberian-husky.jpg" alt="Los Angeles" width="1100"> -->
       <div class="slider-1">
-        <h3 class="title-ped">Cotizaciones</h3>
-        <!-- <p>Cómo realizar pedidos</p> -->
+        <img src="image/custom/cotizacionfondo.jpg" alt="">
+        <h3 class="title-ped">¿Cómo realizar cotizaciones?</h3>
+
       </div>
     </div>
     <div class="carousel-item">
-      <!-- <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/Siberian-husky.jpg" alt="Chicago" width="1100"> -->
       <div class="slider-2">
-        <h3 class="title-ped">Paso 1</h3>
-        <!-- <p>Lorem ipsum lorem ipsum lorem ipsum</p> -->
+        <div class="cont--img">
+          <img src="image/custom/guia9.png">
+        </div>
+        <div class="cont--title">
+          <h3 class="title-ped1">Paso 1</h3>
+          <p class="explicacion-cot">Para realizar una cotización, deberás diligenciar el formulario para continuar con el proceso.</p>
+        </div>
       </div>
     </div>
+
     <div class="carousel-item">
-      <!-- <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/Siberian-husky.jpg" alt="New York" width="1100"> -->
       <div class="slider-3">
-        <h3 class="title-ped">Paso 2</h3>
-        <!-- <p>Lorem ipsum lorem ipsum lorem ipsum</p> -->
+        <div class="cont--img">
+          <img src="image/custom/guia10.png">
+        </div>
+        <div class="cont--title">
+          <h3 class="title-ped1">Paso 2</h3>
+          <p class="explicacion-cot">Si aún no conoces la referencia del producto que deseas cotizar, podrás acceder a esta sección para que puedas encontrar tus productos por marca, palabras claves, tipo de producto, características, etc… Posterior a esto, dar clic en “Buscar” y en el botón “Agregar”.</p>
+        </div>
       </div>
     </div>
+
+    <div class="carousel-item">
+      <div class="slider-2">
+        <div class="cont--img">
+          <img src="image/custom/guia11.png">
+        </div>
+        <div class="cont--title">
+          <h3 class="title-ped1">Paso 3</h3>
+          <p class="explicacion-cot">Para continuar con tu proceso de cotización, deberás terminar de diligenciar el formulario, con la posibilidad de dar clic en “Otro producto” para agregar uno nuevo o en “Terminar” para acabar con tu solicitud.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <div class="slider-2">
+        <div class="cont--img">
+          <img src="image/custom/guia12.png">
+        </div>
+        <div class="cont--title">
+          <h3 class="title-ped1">Paso 4</h3>
+          <p class="explicacion-cot">Tendrás la posibilidad de cambiar la dirección y ver los detalles de tu cotización. Finalmente, dar clic en “Realizar Cotización” para obtener el código de la cotización realizada.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <div class="slider-2">
+        <div class="cont--img">
+          <img src="image/custom/guia13.png">
+        </div>
+        <div class="cont--title">
+          <h3 class="title-ped1">Paso 5</h3>
+          <p class="explicacion-cot">En esta sección obtendrás el código de tu cotización. Podrás ver todos los pedidos y cotizaciones realizadas.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <div class="slider-2">
+        <div class="cont--img">
+          <img src="image/custom/guia14.png">
+        </div>
+        <div class="cont--title">
+          <h3 class="title-ped1">Paso 6</h3>
+          <p class="explicacion-cot">En esta sección podrás ver los detalles de la cotización y eliminarla si aún la cotización se encuentra en su estado “recepción”.</p>
+      </div>
+    </div>
+
+
   </div>
   <a class="carousel-control-prev" href="#slider1" data-slide="prev">
     <span class="carousel-control-prev-icon"></span>
@@ -127,36 +191,98 @@
 </div>
 
 <div class="three"><p></p></div>
+<!---->
 
 
+<!-- PEDIDOS-->
 <div id="slider2" class="carousel slide" data-ride="carousel">
   <ul class="carousel-indicators">
     <li data-target="#slider2" data-slide-to="0" class="active"></li>
     <li data-target="#slider2" data-slide-to="1" ></li>
     <li data-target="#slider2" data-slide-to="2"></li>
+    <li data-target="#slider2" data-slide-to="3"></li>
+    <li data-target="#slider2" data-slide-to="4"></li>
+    <li data-target="#slider2" data-slide-to="5"></li>
   </ul>
   <div class="carousel-inner" >
     <div class="carousel-item active">
-      <!-- <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/Siberian-husky.jpg" alt="Los Angeles" width="1100"> -->
       <div class="slider-1">
-        <h3 class="title-ped">Pedidos</h3>
-        <!-- <p>Cómo realizar pedidos</p> -->
+        <img src="image/custom/pedidofondo.jpg" alt="">
+        <h3 class="title-ped">¿Cómo realizar Pedidos?</h3>
       </div>
     </div>
     <div class="carousel-item">
-      <!-- <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/Siberian-husky.jpg" alt="Chicago" width="1100"> -->
       <div class="slider-2">
-        <h3 class="title-ped">Paso 1</h3>
-        <!-- <p>Lorem ipsum lorem ipsum lorem ipsum</p> -->
+        <div class="cont--img">
+          <img src="image/custom/guia2.png">
+        </div>
+        <div class="cont--title">
+          <h3 class="title-ped1">Paso 1</h3>
+          <p class="explicacion-cot">Para realizar un pedido, deberás diligenciar el formulario para continuar con el proceso.</p>
+        </div>
       </div>
     </div>
+
     <div class="carousel-item">
-      <!-- <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/Siberian-husky.jpg" alt="New York" width="1100"> -->
       <div class="slider-3">
-        <h3 class="title-ped">Paso 2</h3>
-        <!-- <p>Lorem ipsum lorem ipsum lorem ipsum</p> -->
+        <div class="cont--img">
+          <img src="image/custom/guia3.png">
+        </div>
+        <div class="cont--title">
+          <h3 class="title-ped1">Paso 2</h3>
+          <p class="explicacion-cot">Si aún no conoces la referencia del producto que deseas solicitar podrás acceder a esta sección para que puedas encontrar por marca, palabras claves, tipo de producto, características, etc… Posterior a esto, dar clic en “Buscar” y en el botón “Agregar”.</p>
+        </div>
       </div>
     </div>
+
+    <div class="carousel-item">
+      <div class="slider-2">
+        <div class="cont--img">
+          <img src="image/custom/guia4.png">
+        </div>
+        <div class="cont--title">
+          <h3 class="title-ped1">Paso 3</h3>
+          <p class="explicacion-cot">Para continuar con tu proceso de pedido, deberás terminar de diligenciar el formulario, con la posibilidad de dar clic en “Otro producto” para agregar uno nuevo o en “Terminar” para acabar con tu solicitud.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <div class="slider-2">
+        <div class="cont--img">
+          <img src="image/custom/guia6.png">
+        </div>
+        <div class="cont--title">
+          <h3 class="title-ped1">Paso 4</h3>
+          <p class="explicacion-cot">En este paso, deberás terminar de diligenciar la fecha de entrega y la hora aproximada del envío. También tendrás la posibilidad de cambiar la dirección y ver el pedido para conocer los detalles. Finalmente, dar clic en “Realizar Pedido” para obtener el código del pedido realizado.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <div class="slider-2">
+        <div class="cont--img">
+          <img src="image/custom/guia7.png">
+        </div>
+        <div class="cont--title">
+          <h3 class="title-ped1">Paso 5</h3>
+          <p class="explicacion-cot">En esta sección obtendrás el código de tu pedido. Podrás ver todos los pedidos y cotizaciones realizadas.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <div class="slider-2">
+        <div class="cont--img">
+          <img src="image/custom/guia8.png">
+        </div>
+        <div class="cont--title">
+          <h3 class="title-ped1">Paso 6</h3>
+          <p class="explicacion-cot">En esta sección podrás ver los detalles del pedido y eliminarlo si aún el pedido se encuentra en su estado “pendiente”.</p>
+      </div>
+    </div>
+
+
   </div>
   <a class="carousel-control-prev" href="#slider2" data-slide="prev">
     <span class="carousel-control-prev-icon"></span>
@@ -165,6 +291,177 @@
     <span class="carousel-control-next-icon"></span>
   </a>
 </div>
+
+<div class="three"><p></p></div>
+<!---->
+
+<!-- CONTACTO -->
+<div id="slider3" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#slider3" data-slide-to="0" class="active"></li>
+    <li data-target="#slider3" data-slide-to="1"></li>
+  </ul>
+  <div class="carousel-inner" >
+    <div class="carousel-item active">
+      <div class="slider-1">
+        <img src="image/custom/contactofondo.jpg">
+        <h3 class="title-ped">Contactar la empresa</h3>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="slider-2">
+        <div class="cont--img">
+          <img src="image/custom/guia16.png">
+        </div>
+        <div class="cont--title">
+          <h3 class="title-ped1">Paso 1</h3>
+          <p class="explicacion-cot">Para comunicarte con la empresa Maxirecargas, deberás diligenciar los campos necesarios y clic en “Enviar”. Además, podrás visualizar los datos de la empresa como lo son: teléfonos, whatsapp, correo y dirección.</p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+  <a class="carousel-control-prev" href="#slider3" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#slider3" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+<div class="three"><p></p></div>
+
+<!--  -->
+
+
+<!-- PERFIL -->
+
+<div id="slider4" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#slider4" data-slide-to="0" class="active"></li>
+    <li data-target="#slider4" data-slide-to="1" ></li>
+    <li data-target="#slider4" data-slide-to="2"></li>
+
+  </ul>
+  <div class="carousel-inner" >
+    <div class="carousel-item active">
+      <div class="slider-1">
+        <img src="image/custom/perfilfondo.jpg" alt="">
+        <h3 class="title-ped">Mi Perfil</h3>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="slider-2">
+        <div class="cont--img">
+          <img src="image/custom/guia17.png">
+        </div>
+        <div class="cont--title">
+          <h3 class="title-ped1">Paso 1</h3>
+          <p class="explicacion-cot">En esta sección, podrás editar tus datos personales. Deberás dar clic en tu nombre y posterior a esto, clic en “Perfil”.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <div class="slider-3">
+        <div class="cont--img">
+          <img src="image/custom/guia18.png">
+        </div>
+        <div class="cont--title">
+          <h3 class="title-ped1">Paso 2</h3>
+          <p class="explicacion-cot">En esta sección podrás editar tus datos personales. Solo debes digitar los campos que desees y clic en “Guardar cambios”.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <div class="slider-2">
+        <div class="cont--img">
+          <img src="image/custom/guia19.png">
+        </div>
+        <div class="cont--title">
+          <h3 class="title-ped1">Paso 3</h3>
+          <p class="explicacion-cot">En esta sección podrás cambiar tu contraseña. Debes llenar los campos necesarios y clic en “Guardar cambios”.</p>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+  <a class="carousel-control-prev" href="#slider4" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#slider4" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+<div class="three"><p></p></div>
+<!---->
+
+
+
+<!-- CHAT -->
+
+<div id="slider5" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#slider5" data-slide-to="0" class="active"></li>
+    <li data-target="#slider5" data-slide-to="1" ></li>
+
+
+  </ul>
+  <div class="carousel-inner" >
+    <div class="carousel-item active">
+      <div class="slider-1">
+        <img src="image/custom/chatfondo.jpg" alt="">
+        <h3 class="title-ped">¿Cómo enviar mensajes?</h3>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="slider-2">
+        <div class="cont--img">
+          <img src="image/custom/guia20.png">
+        </div>
+        <div class="cont--title">
+          <h3 class="title-ped1">Paso 1</h3>
+          <p class="explicacion-cot">En la parte inferior derecha podrás acceder al chat para comunicarte directamente con un asesor virtual de la empresa. Solo debes dar clic en “Online Maxirecargas”.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <div class="slider-3">
+        <div class="cont--img">
+          <img src="image/custom/guia21.png">
+        </div>
+        <div class="cont--title">
+          <h3 class="title-ped1">Paso 2</h3>
+          <p class="explicacion-cot">Al dar clic, aparecerá el chat para enviar el mensaje al asistente virtual. Si deseas terminar con la conversación solo debes dar clic en “Finalizar”.</p>
+        </div>
+      </div>
+    </div>
+
+
+
+  </div>
+  <a class="carousel-control-prev" href="#slider5" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#slider5" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+<div class="three"><p></p></div>
+<!---->
+
+
+
+
+
+
+
+
 
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
