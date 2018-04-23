03c81e41d9f6090ef9ba73b2c8340b056041627a
estefatapias99 <32174302+estefatapias99@users.noreply.github.com>
2018-04-23 14:26:30 -0500

    ....

--- views/productos.php
+++ views/productos.php
@@ -49,6 +49,6 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script type="text/javascript" src="main.js"></script>
+  <script type="text/javascript" src="..main.js"></script>
   </body>
 </html>
